4048eb17d2df74ec5bf02c622d1cb9ea2a80b4b5
Derrick Strong <derrickmstrong@gmail.com>
2021-09-23 11:15:46 -0500

    Added Streams to StreamsList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../store/actions';
+import { Link } from 'react-router-dom';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+const StreamList = ({ streams, getAllStreams, currentUserId, isSignedIn }) => {
+  console.log(streams, getAllStreams, currentUserId);
+  useEffect(() => {
+    getAllStreams();
+    return () => {};
+  }, []);
+
+  const renderAdmin = stream => {
+    if (stream.userId === currentUserId) {
+      return (
+        <div className='right floated content'>
+          <button className='ui button primary'>Edit</button>
+          <button className='ui button negative'>Delete</button>
+        </div>
+      );
+    }
+  };
+
+  const renderCreate = () => {
+    if (isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right'}}>
+          <Link to='/streams/new' className='ui button primary'>
+            Create New Stream
+          </Link>
+        </div>
+      );
+    }
+  };
+
+  const renderList = streams.map(stream => {
+    return (
+      <div className='item' key={stream.id}>
+        {/* In otder for Semantic UI to format button correctly, renderAdmin(stream) has to be at top */}
+        {renderAdmin(stream)}
+        <i className='large middle aligned icon camera' />
+        <div className='content'>
+          {stream.title}
+          <div className='description'>{stream.description}</div>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className='ui celled list'>{renderList}</div>
+      {renderCreate()}
+    </div>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams), // Turns object values into an array; map through prop
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllStreams: () => dispatch(fetchStreams()), // dispatch as prop on page load
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(StreamList);
