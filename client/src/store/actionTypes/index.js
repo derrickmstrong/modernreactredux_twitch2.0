6f0d27141ae35aecc380e557d2eb492f73e33f04
Derrick Strong <derrickmstrong@gmail.com>
2021-09-23 08:09:39 -0500

    Added action types + creators for CRUD ops

--- client/src/store/actionTypes/index.js
+++ client/src/store/actionTypes/index.js
@@ -1,2 +1,10 @@
 export const SIGN_IN = 'SIGN_IN'
 export const SIGN_OUT = 'SIGN_OUT'
+
+export const CREATE_STREAM = 'CREATE_STREAM' // create stream
+export const FETCH_STREAMS = 'FETCH_STREAMS' // get all streams
+export const FETCH_STREAM = 'FETCH_STREAM' // get one stream
+export const DELETE_STREAM = 'DELETE_STREAM' // delete stream
+export const EDIT_STREAM = 'EDIT_STREAM' // edit stream
+
+
